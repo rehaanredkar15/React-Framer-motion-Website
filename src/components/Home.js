8e8486cbe4007b780f546e47e194b4e2ab79bf57
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-04-24 01:45:13 +0530

    test - used the initatial with animate

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const Home = () => {
-  return (
-    <div className="home container">
-      {/* here motion is added to the component and in animate function we add the property animate to it and pass the props which we want to animate  */}
-      <motion.h2 animate={{ fontSize: 50, color: "#ff2994", x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
-      <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
-      </Link>
-    </div>
-  );
+    return ( <
+        motion.div className = "home container"
+        initial = {
+            { opacity: 0 } }
+        animate = {
+            { opacity: 1 } } >
+        { /* here motion is added to the component and in animate function we add the property animate to it and pass the props which we want to animate  */ } <
+        motion.h2 animate = {
+            { fontSize: 50, color: "#ff2994", x: 100, y: -100 } } >
+        Welcome to Pizza Joint <
+        /motion.h2> <
+        Link to = "/base" >
+        <
+        motion.button animate = {
+            { scale: 1.5 } } >
+        Create Your Pizza <
+        /motion.button> <
+        /Link> <
+        /motion.div>
+    );
 };
 
 export default Home;
