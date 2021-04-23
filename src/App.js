8a1892f364f21b7197f116bd44465e06475eeb19
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-04-23 16:23:14 +0530

    docs - Added the comments explaining the flow of the program
    Explained the flow of the program and how the react state changes

--- src/App.js
+++ src/App.js
@@ -7,57 +7,44 @@ import Toppings from "./components/Toppings";
 import Order from "./components/Order";
 
 function App() {
-    const [pizza, setPizza] = useState({ base: "", toppings: [] }); // to know the current state of the pizza that is its base and toppings
+  const [pizza, setPizza] = useState({ base: "", toppings: [] }); // to know the current state of the pizza that is its base and toppings
 
-    const addBase = (base) => {
-        setPizza({...pizza, base }); //setting up the base
-    };
+  const addBase = (base) => {
+    setPizza({ ...pizza, base }); //setting up the base
+  };
 
-    //adding topppings if the toppings are not present than we add it
-    const addTopping = (topping) => {
-        let newToppings;
-        if (!pizza.toppings.includes(topping)) {
-            newToppings = [...pizza.toppings, topping]; //we take the values of toppings in new toppings and have spreaded pizza which is the current state of our pizza
-        } else {
-            newToppings = pizza.toppings.filter((item) => item !== topping);
-        }
-        setPizza({...pizza, toppings: newToppings });
-    };
+  //adding topppings if the toppings are not present than we add it
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping]; //we take the values of toppings in new toppings and have spreaded pizza which is the current state of our pizza
+    } else {
+      newToppings = pizza.toppings.filter((item) => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  };
 
-    //we call this functions in the routing which is below and we pass the pizza which has base added and toppings added and befor that this functions are called
+  //we call this functions in the routing which is below and we pass the pizza which has base added and toppings added and befor that this functions are called
 
-    return ( <
-        >
-        <
-        Header / >
-        <
-        Switch >
-        <
-        Route path = "/base" >
-        <
-        Base addBase = { addBase }
-        pizza = { pizza }
-        /> <
-        /Route> <
-        Route path = "/toppings" >
-        <
-        Toppings addTopping = { addTopping }
-        pizza = { pizza }
-        /> <
-        /Route> <
-        Route path = "/order" >
-        <
-        Order pizza = { pizza }
-        /> <
-        /Route> <
-        Route path = "/" >
-        <
-        Home / >
-        <
-        /Route> <
-        /Switch> <
-        />
-    );
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/base">
+          <Base addBase={addBase} pizza={pizza} />
+        </Route>
+        <Route path="/toppings">
+          <Toppings addTopping={addTopping} pizza={pizza} />
+        </Route>{" "}
+        <Route path="/order">
+          <Order pizza={pizza} />{" "}
+        </Route>{" "}
+        <Route path="/">
+          <Home />
+        </Route>{" "}
+      </Switch>{" "}
+    </>
+  );
 }
 
 export default App;
