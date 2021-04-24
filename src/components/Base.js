31751ccbe3f9b0205213fcacdb2cec483e05a45f
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-04-24 12:59:02 +0530

    feat - Added animation to the base component
    Animated the base page and also learnt about the stiffness and spring property

--- src/components/Base.js
+++ src/components/Base.js
@@ -6,7 +6,12 @@ const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <div className="base container">
+    <motion.div
+      className="base container"
+      initial={{ y: "100vw" }}
+      animate={{ y: 0 }}
+      transition={{ type: "spring", delay: 0.5, stiffness: 50 }}
+    >
       <h3> Step 1: Choose Your Base </h3>{" "}
       <ul>
         {" "}
@@ -25,13 +30,14 @@ const Base = ({ addBase, pizza }) => {
           className="next"
           initial={{ x: "-100vw" }}
           animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 100 }}
         >
           <Link to="/toppings">
             <button> Next </button>{" "}
           </Link>{" "}
         </motion.div>
       )}{" "}
-    </div>
+    </motion.div>
   );
 };
 
