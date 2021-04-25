19f48290d65cd3aa2151887a9a51efa1ebfc3f69
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-04-25 13:19:25 +0530

    feat - animated the order page

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useStae } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+const containerVariants = {
+    hidden: {
+        opacity: 0,
+        x: "100vw",
+        transition: {
+            staggerChildren: 0.5,
+        },
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            type: "spring",
+            mass: 0.4,
+            damping: 8,
+            staggerChildren: 0.4,
+            when: "beforeChildren",
+        },
+    },
+};
+
+const childVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+    },
+};
 
 const Order = ({ pizza }) => {
-  return (
-    <div className="container order">
-      <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+    return ( <
+            motion.div className = "container order"
+            variants = { containerVariants }
+            initial = "hidden"
+            animate = "visible" >
+            <
+            h2 > Thank you
+            for your order: ) < /h2>{" "} <
+        motion.p variants = { childVariants } >
+        You ordered a { pizza.base }
+    pizza with:
+        <
+        /motion.p>{" "} <
+        motion.div variants = { childVariants } > { " " } {
+            pizza.toppings.map((topping) => ( <
+                div key = { topping } > { topping } < /div>
+            ))
+        } { " " } <
+        /motion.div>{" "} <
+        /motion.div>
+);
+};
 
 export default Order;
